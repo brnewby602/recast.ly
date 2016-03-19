0fc9e7eafdb0b9dfe19e7f7bf1b5128cf78587cb
brnewby602 <brnewby602@gmail.com>
2016-03-18 17:08:27 -0700

    Refactor to use React Component and ES6 syntax

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,23 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+
+  constructor(props) {
+    super(props); 
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer/>
+        </div>
+        <div className="col-md-5">
+          <VideoList list={this.props.videoData}/>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+ReactDOM.render(<App videoData={window.exampleVideoData}/>, document.getElementById('app'));
